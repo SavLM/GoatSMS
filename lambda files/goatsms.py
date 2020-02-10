18170c7e2ab97b2bf923f05066cf2595d9d85c82
SavLM <50849597+SavLM@users.noreply.github.com>
2020-02-10 18:11:31 -0500

    moved project to its own repository

---
+++ lambda files/goatsms.py
@@ -0,0 +1,33 @@
+from twilio.rest import Client
+import os
+from random import randint
+import boto3
+
+# Your Account Sid and Auth Token from twilio.com/console
+account_sid = os.environ.get("TWILIO_ACCOUNT_SID")
+auth_token = os.environ.get("TWILIO_AUTH_TOKEN")
+client = Client(account_sid, auth_token)
+
+def change_time():
+    value = randint(12,17)
+    schedule = 'cron(0 ' + str(value) + ' * * ? *)'
+    sched = boto3.client('events')
+    sched.put_rule(
+        Name='goattime',
+        ScheduleExpression=schedule,
+    )
+    return schedule
+
+def lambda_handler(event, context):
+    ret = ''
+    numbers = ["+15408399215","+14346889846","+12526730188", "+14084274768"]
+    for number in numbers:
+        message = client.messages \
+            .create(
+                body="\n-\n- \n \nA goat!!! \n\n- From your friendly, neighborhood Savvy",
+                from_="+12092059061",
+                media_url=["https://placegoat.com/200/200"],
+                to=number
+            )
+        ret += message.sid + ' '
+    return change_time()
